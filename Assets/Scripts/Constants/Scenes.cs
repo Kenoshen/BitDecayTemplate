85c426a4ef351e3a1687c071046d6bbe4e45ab34
Tanner <Shadoninja@gmail.com>
2019-01-01 17:58:02 -0700

    Splash screen in

--- Assets/Scripts/Constants/Scenes.cs
+++ Assets/Scripts/Constants/Scenes.cs
@@ -3,9 +3,15 @@ public class Scenes
 {
 	public const string DontDestroyOnLoad = "DontDestroyOnLoad";
 	public const string SampleScene = "SampleScene";
+	public const string SceneOne = "SceneOne";
+	public const string SceneTwo = "SceneTwo";
+	public const string BitdecaySplash = "BitdecaySplash";
 	public enum SceneEnum
 	{
 		SampleScene = 80,
+		SceneOne = 16,
+		SceneTwo = 40,
+		BitdecaySplash = 144,
 	}
 	public static string GetSceneNameFromEnum(SceneEnum sceneEnum)
 	{
@@ -13,6 +19,12 @@ public class Scenes
 		{
 			case SceneEnum.SampleScene:
 				return SampleScene;
+			case SceneEnum.SceneOne:
+				return SceneOne;
+			case SceneEnum.SceneTwo:
+				return SceneTwo;
+			case SceneEnum.BitdecaySplash:
+				return BitdecaySplash;
 			default:
 				throw new Exception("Unable to resolve scene name for: " + sceneEnum);
 		}
