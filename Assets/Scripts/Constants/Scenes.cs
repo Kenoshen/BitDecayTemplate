e4e431c0d5c4d84af99d7a7b85eb74651c6a553a
Tanner <Shadoninja@gmail.com>
2019-01-01 18:59:59 -0700

    cleaned up script organization

--- Assets/Scripts/Constants/Scenes.cs
+++ Assets/Scripts/Constants/Scenes.cs
@@ -6,12 +6,18 @@ public class Scenes
 	public const string BitdecaySplash = "BitdecaySplash";
 	public const string GameJamSplash = "GameJamSplash";
 	public const string Credits = "Credits";
+	public const string BitdecaySplash = "BitdecaySplash";
+	public const string GameJamSplash = "GameJamSplash";
+	public const string BitdecaySplash = "BitdecaySplash";
 	public enum SceneEnum
 	{
 		TitleScreen = 98,
 		BitdecaySplash = 144,
 		GameJamSplash = 253,
 		Credits = 206,
+		BitdecaySplash = 144,
+		GameJamSplash = 253,
+		BitdecaySplash = 144,
 	}
 	public static string GetSceneNameFromEnum(SceneEnum sceneEnum)
 	{
@@ -25,6 +31,12 @@ public class Scenes
 				return GameJamSplash;
 			case SceneEnum.Credits:
 				return Credits;
+			case SceneEnum.BitdecaySplash:
+				return BitdecaySplash;
+			case SceneEnum.GameJamSplash:
+				return GameJamSplash;
+			case SceneEnum.BitdecaySplash:
+				return BitdecaySplash;
 			default:
 				throw new Exception("Unable to resolve scene name for: " + sceneEnum);
 		}
